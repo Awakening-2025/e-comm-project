b01421051e906120f41c8cde96e4acfc4973ff0b
averma0001 <averma.sre@gmail.com>
2025-05-27 16:12:16 +0530

    add title nd icon in tab bar

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App.jsx'
+import favicon from './assets/websitelogo.jpeg';
+
+
+const setFavicon = (url) => {
+  const link = document.createElement('link');
+  link.rel = 'icon';
+  link.type = 'image/jpeg'; // Change to image/png if your favicon is PNG
+  link.href = url;
+  document.head.appendChild(link);
+};
+
+setFavicon(favicon);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
